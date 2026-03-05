760062deb3aa0cc1765727f61a684191d311c098
MOiher2710 <Yoxanderh@gmail.com>
2026-03-05 11:50:49 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/EvaluadorCorales3.1/",
+  base: "/EvaluadorCorales3.2/",
   plugins: [react()],
 });
